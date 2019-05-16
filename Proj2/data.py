82b7d93c099f6101e763c9ad8399e0f4f4c8c4ac
Gregoire Clement <gregoire.clement@epfl.ch>
2019-05-16 23:56:42 +0200

    fix the data generation

--- Proj2/data.py
+++ Proj2/data.py
@@ -8,7 +8,7 @@ DATASET_SIZE = 1000
 
 def generate_disc_set(nb):
     inputs = Tensor(nb, 2).uniform_(0, 1)
-    targets = inputs.pow(2).sum(1).sub(2 / math.pi).sign().add(1).div(2).long()
+    targets = (inputs * 2 - 1).pow(2).sum(1).sub(2 / math.pi).sign().add(1).div(2).long()
     return inputs, targets
 
 
